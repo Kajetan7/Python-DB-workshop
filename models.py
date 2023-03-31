3ddaf73c2b2e53096c6f19646338bc159e561f9f
Kajetan7 <krmroske@gmail.com>
2023-03-31 14:12:24 +0200

    classes for db service

---
+++ models.py
@@ -0,0 +1,18 @@
+from clcrypto import hash_password
+
+
+class User:
+    def __init__(self, username='', password='', salt=''):
+        self._id = -1
+        self.username = username
+        self._hashed_password = hash_password(password, salt)
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def hashed_password(self):
+        return self._hashed_password
+
+
