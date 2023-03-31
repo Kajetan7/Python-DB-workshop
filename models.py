c669f0ded38ee6eb42a2c53fef18e857784ba44e
Kajetan7 <krmroske@gmail.com>
2023-03-31 16:24:21 +0200

    check: adding new user to db

--- models.py
+++ models.py
@@ -1,4 +1,12 @@
-from clcrypto import hash_password
+from pw_encryption import generate_salt, hash_password, check_password
+from psycopg2 import connect, sql, OperationalError
+
+try:
+    cnx = connect(database="workshop", user='postgres', password='coderslab', host='localhost', port=5432)
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+except OperationalError as e:
+    print("Connection Error: ", e)
 
 
 class User:
@@ -21,3 +29,17 @@ class User:
     @hashed_password.setter
     def hashed_password(self, password):
         self._hashed_password = password
+
+    def save_to_db(self, cursor):
+        if self._id == -1:
+            sql = '''INSERT INTO users (username, hashed_password)
+                        VALUES (%s, %s) RETURNING id'''
+            values = (self.username, self.hashed_password)
+            cursor.execute(sql, values)
+            self._id = cursor.fetchone()[0]
+            return True
+        return False
+
+#  check: adding new user to database
+new_user = User('kajetan', 'kk33')
+new_user.save_to_db(cursor)
