f0dc7a4da33c30fdef6dde6b4a16954530500f3d
Kajetan7 <krmroske@gmail.com>
2023-04-07 16:26:54 +0200

    first issue of messages module

---
+++ messages.py
@@ -0,0 +1,34 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--username", help="username")
+parser.add_argument("-p", "--password", help="password")
+parser.add_argument("-l", "--list", help="list all messages", action="store_true")
+parser.add_argument("-t", "--to", help="to")
+parser.add_argument("-s", "--send", help="text message to send")
+
+args = parser.parse_args()
+
+
+def print_user_messages(cur, user):
+    messages = Message.load_all_messages(cur, user.id)
+    for message in messages:
+        from_ = User.load_user_by_id(cur, message.from_id)
+        print(20 * "-")
+        print(f'from: {from_.username}')
+        print(f'date: {message.creation_date}')
+        print(message.text)
+        print(20 * "-")
+
+
+def send_message(cur, from_id, recipent_name, text):
+    if len(text) > 255:
+        print("Message is too long!")
+        return
+    to = User.load_user_by_username(cur, recipient_name)
+    if to:
+        message = Message(from_id, to.id, text=text)
+        message.save_to_db(cur)
+        print("Message sent.")
+    else:
+        print("Recipient does not exists.")
