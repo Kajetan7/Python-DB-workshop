dcb998560ada05d3fe18b9e74804eb8adc8c67ca
Kajetan7 <krmroske@gmail.com>
2023-04-07 14:56:48 +0200

    class Messages added

--- models.py
+++ models.py
@@ -46,6 +46,17 @@ class User:
             return True
 
     @staticmethod
+    def load_user_by_username(cursor, username):
+        sql = "SELECT id, username, hashed_password FROM users WHERE username=%s"
+        cursor.execute(sql, (username,))
+        data = cursor.fetchone()
+        if data:
+            id_, username, hashed_password = data
+            loaded_user = User(username)
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
+            return loaded_user
+    @staticmethod
     def load_user_by_id(cursor, id_):
         sql = "SELECT id, username, hashed_password FROM users WHERE id=%s"
         cursor.execute(sql, (id_,))
@@ -76,6 +87,55 @@ class User:
         cursor.execute(sql, (self.id,))
         self._id = -1
         return True
+
+
+class Message:
+    def __init__(self, from_id, to_id, text):
+        self._id = -1
+        self.from_id = from_id
+        self.to_id = to_id
+        self.text = text
+        self._creation_date = None
+
+    @property
+    def creation_date(self):
+        return self._creation_date
+
+    @property
+    def id(self):
+        return self._id
+
+    @staticmethod
+    def load_all_messages(cursor, user_id=None):
+        if user_id:
+            sql = "SELECT id, from_id, to_id, text, creation_date FROM messages WHERE to_id=%s"
+            cursor.execute(sql, (user_id,))
+        else:
+            sql = "SELECT id, from_id, to_id, text, creation_date FROM messages"
+            cursor.execute(sql)
+        messages = []
+        for row in cursor.fetchall():
+            id_, from_id, to_id, text, creation_date = row
+            loaded_message = Message(from_id, to_id, text)
+            loaded_message._id = id_
+            loaded_message._creation_date = creation_date
+            messages.append(loaded_message)
+        return messages
+
+    def save_to_db(self, cursor):
+        if self._id == -1:
+            sql = """INSERT INTO Messages (from_id, to_id, text)
+                            VALUES (%s, %s, %s) RETURNING id, creation_date"""
+            values = (self.from_id, self.to_id, self.text)
+            cursor.execute(sql, values)
+            self._id, self._creation_date = cursor.fetchone()
+            return True
+        else:
+            sql = "UPDATE Messages SET to_id=%s, from_id=%s, text=%s WHERE id=%s"
+            values = (self.from_id, self.to_id, self.text, self.id)
+            cursor.execute(sql, values)
+            return True
+
 #  check: adding new user to database
 new_user = User('kajetan', 'kk33')
 new_user.save_to_db(cursor)
