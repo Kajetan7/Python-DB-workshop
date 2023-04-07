f779ac5ea3c4a5d3a6c8cdc60da87bde0eca25ee
Kajetan7 <krmroske@gmail.com>
2023-04-07 11:55:51 +0200

    added new methods to models

--- models.py
+++ models.py
@@ -28,7 +28,7 @@ class User:
 
     @hashed_password.setter
     def hashed_password(self, password):
-        self._hashed_password = password
+        self.set_password(password)
 
     def save_to_db(self, cursor):
         if self._id == -1:
@@ -38,8 +38,44 @@ class User:
             cursor.execute(sql, values)
             self._id = cursor.fetchone()[0]
             return True
-        return False
+        else:
+            sql = '''UPDATE Users SET username=%s, hashed_password=%s
+                        WHERE id=%s'''
+            values = (self.username, self.hashed_password, self.id)
+            cursor.execute(sql, values)
+            return True
 
+    @staticmethod
+    def load_user_by_id(cursor, id_):
+        sql = "SELECT id, username, hashed_password FROM users WHERE id=%s"
+        cursor.execute(sql, (id_,))
+        data = cursor.fetchone()
+        if data:
+            id_, username, hashed_password = data
+            loaded_user = User(username)
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
+            return loaded_user
+
+    @staticmethod
+    def load_all_users(cursor):
+        sql = "SELECT id, username, hashed_passowrd FROM Users"
+        users = []
+        cursor.execute(sql)
+        for row in cursor.fetchall():
+            id_, username, hashed_password = row
+            loaded_user = User()
+            loaded_user._id = id_
+            loaded_user.username = username
+            loaded_user._hashed_password = hashed_password
+            users.append(loaded_user)
+        return users
+
+    def delete(self, cursor):
+        sql = "DELETE FROM Users WHERE id=%s"
+        cursor.execute(sql, (self.id,))
+        self._id = -1
+        return True
 #  check: adding new user to database
 new_user = User('kajetan', 'kk33')
 new_user.save_to_db(cursor)
