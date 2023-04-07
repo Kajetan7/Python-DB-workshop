07ef60081d13ab0089fd26cd855f510eddcaa3f0
Kajetan7 <krmroske@gmail.com>
2023-04-07 16:07:00 +0200

    Rest of functionalities added

--- users.py
+++ users.py
@@ -1,5 +1,7 @@
 import argparse
 from models import User
+from psycopg2.errors import UniqueViolation
+from pw_encryption import check_password
 
 
 parser = argparse.ArgumentParser()
@@ -12,7 +14,47 @@ parser.add_argument("-e", "--edit", help="edit user", action="store_true")
 
 args = parser.parse_args()
 
+
 def list_users(cur):
     users = User.load_all_users(cur)
     for user in users:
         print(user.username)
+
+
+def create_user(cur, username, password):
+    if len(password) < 8:
+        print('Password is too short. It should have minimum 8 characters.')
+    else:
+        try:
+            user = User(username=username, password=password)
+            user.save_to_db(cur)
+            print('User created')
+        except UniqueViolation as e:
+            print("User already exists. ", e)
+
+
+def delete_user(cur, username, password):
+    user = User.load_user_by_username(cur, username)
+    if not user:
+        print("User does not exist!")
+    elif check_password(password, user.hashed_password):
+        user.delete(cur)
+        print("User deleted.")
+    else:
+        print("Incorrect password!")
+
+
+def edit_user(cur, username, password, new_pass):
+    user = User.load_user_by_username(cur, username)
+    if not user:
+        print("User does not exist!")
+    elif check_password(password, user.hashed_password):
+        if len(new_pass) < 8:
+            print("Password is too short. It should have minimum 8 characters.")
+        else:
+            user.hashed_password = new_pass
+            user.save_to_db(cur)
+            print("Password changed.")
+    else:
+        print("Incorrect password.")
+
